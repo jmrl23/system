c163ca8de22f11714c3d491a20f7b272da137946
Jomariel Gaitera <jojogaitera@gmail.com>
2022-10-22 23:10:10 +0800

    Make rate-limiter middleware configurable
    We might want to limit different routes with different rates,
    instead of using the middleware as it is, I made it returns a callback
    instead that has default configuration and will alter if an option is set
    manually.

--- src/middlewares/rate-limiter.ts
+++ src/middlewares/rate-limiter.ts
@@ -1,10 +1,23 @@
 import limiter from 'express-rate-limit'
+import type { Options } from 'express-rate-limit'
+import type { Request, Response } from 'express'
 
-const rateLimiter = limiter({
-  windowMs: 5 * 60 * 1000,
-  max: 120,
-  standardHeaders: true,
-  legacyHeaders: false
-})
+const rateLimiter = (passedOptions: Partial<Options> = {}) => {
+  return limiter({
+    windowMs: 5 * 60 * 1000,
+    max: 120,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: async (request: Request, response: Response) => {
+      const statusCode = request.statusCode ?? 429
+      return response.status(statusCode).json({
+        statusCode,
+        message: `Cannot ${request.method} ${request.url}`,
+        error: 'Too Many Requests'
+      })
+    },
+    ...passedOptions
+  })
+}
 
 export { rateLimiter }
