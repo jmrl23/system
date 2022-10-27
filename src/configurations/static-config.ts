f0cbd7644960245fbbd975ac604ec3f5fac1a92e
Jomariel Gaitera <jojogaitera@gmail.com>
2022-10-27 13:00:01 +0800

    Fix bug

--- src/configurations/static-config.ts
+++ src/configurations/static-config.ts
@@ -1,7 +1,9 @@
 import type { ServeStaticOptions } from 'serve-static'
 
 const staticConfig: ServeStaticOptions = {
-    maxAge: process.env.NODE_ENV === 'production' ? '31536000' : 'public, max-age=0'
+    maxAge: process.env.NODE_ENV === 'production' ?
+        'public, max-age=31536000' :
+        'public, max-age=0'
 }
 
 export { staticConfig }
