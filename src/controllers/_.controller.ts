8bbc5453d014a099e3912c36f1fc50a4a7138a11
Jomariel Gaitera <jojogaitera@gmail.com>
2022-10-29 15:48:13 +0800

    Fix typo

--- src/controllers/_.controller.ts
+++ src/controllers/_.controller.ts
@@ -1,19 +1,29 @@
 import { Router } from 'express'
+import type { Request, Response, NextFunction } from 'express'
+import { NotFoundError } from 'express-response-errors'
 
 const controller = Router()
 
 controller
 
-  .get('/', function (request, response) {
-    if (!request.user) return response.redirect('/login')
-    response.render('home', {
-      title: 'System'
+  .get('/',
+    function (request: Request, response: Response) {
+      if (request.isUnauthenticated())
+        return response.redirect('/login')
+      response.render('home', {
+        title: 'System'
+      })
     })
-  })
 
-  .get('/login', function (request, response) {
-    if (request.user) return response.redirect('/')
-    response.render('sign-in')
-  })
+  .get('/login',
+    function (
+      request: Request,
+      response: Response,
+      next: NextFunction
+    ) {
+      if (request.isAuthenticated())
+        return next(new NotFoundError())
+      response.render('sign-in')
+    })
 
 export { controller }
