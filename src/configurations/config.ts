b72807ecf4754a5e7f01aa5259bb650f400c84a7
Jomariel Gaitera <jojogaitera@gmail.com>
2022-11-01 02:55:38 +0800

    Add trust proxy

--- src/configurations/config.ts
+++ src/configurations/config.ts
@@ -4,6 +4,8 @@ export const NODE_ENV = env.get('NODE_ENV').default('development').asEnum(['deve
 
 export const NONCE = env.get('NONCE').required().asString()
 
+export const TRUST_PROXY = env.get('TRUST_PROXY').default('loopback').asString()
+
 export const SESSION_SECRET = env.get('SESSION_SECRET').required().asString()
 
 export const DATABASE_URL = env.get('DATABASE_URL').required().asUrlString()
