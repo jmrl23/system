8af436dd5c7cf9f3c7f968ed1308c4a9208483da
Jomariel Gaitera <jojogaitera@gmail.com>
2022-11-05 02:37:54 +0800

    Fix bug

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -7,16 +7,16 @@ import { UnauthorizedError } from 'express-response-errors'
  * It takes an array of roles and returns a function that takes a request, response, and next function.
  *
  *
- * The returned function checks if the request is unauthenticated or if the user's role is included in
- * the array of roles. If either of those are true, it returns the next function with a new
- * UnauthorizedError. Otherwise, it calls the next function.
+ * The returned function checks if the request is unauthenticated or if the user's role is not included
+ * in the array of roles passed to the authorization function. If either of those conditions are true,
+ * it calls next with a new UnauthorizedError. Otherwise, it calls next.
  * @param {Role[]} role - Role[] - an array of roles that are allowed to access the route
  * @returns A function that takes a request, response, and next function.
  */
 function authorization(role: Role[]) {
   return function (request: Request, _response: Response, next: NextFunction) {
     const user = request.user as SessionUser
-    if (request.isUnauthenticated() || role.includes(user.UserRole.role))
+    if (request.isUnauthenticated() || !role.includes(user.UserRole.role))
       return next(new UnauthorizedError('Access denied'))
     next()
   }
