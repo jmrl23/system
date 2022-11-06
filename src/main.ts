acb7736c60ff512aaa49be5369c3e1c66d39c7d6
Jomariel Gaitera <jojogaitera@gmail.com>
2022-11-07 02:48:01 +0800

    Add profile page

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import clc from 'cli-color'
 import detectPort from 'detect-port'
 
 async function main() {
-  const port = await detectPort(parseInt(process.env.PORT || '3000', 10))
+  const port = await detectPort(parseInt(process.env.PORT ?? '3000', 10))
   process.env.PORT = port.toString()
   server.listen(port, () => {
     console.log(`${clc.bold.bgGreen(' SERVER ')} http://localhost:${port}/\n`)
