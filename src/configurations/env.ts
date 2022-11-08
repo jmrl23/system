44e2d218c1673dcec62256f70cafcec67ad68c21
Jomariel Gaitera <jojogaitera@gmail.com>
2022-11-08 22:45:08 +0800

    Add environment variable `DISABLE_ERD`

--- src/configurations/env.ts
+++ src/configurations/env.ts
@@ -7,6 +7,8 @@ export const NODE_ENV = e
 
 export const PORT = e.get('PORT').default(3000).asPortNumber()
 
+export const DISABLE_ERD = e.get('DISABLE_ERD').required().asBool()
+
 export const IS_MAINTENANCE = e.get('IS_MAINTENANCE').default('false').asBool()
 
 export const API_AUTH_KEY = e.get('API_AUTH_KEY').required().asString()
