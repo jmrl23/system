6cd03944344a3226451dfe84b1ec8a3e1a2061a4
Jomariel Gaitera <jojogaitera@gmail.com>
2022-11-08 09:51:01 +0800

    Add example fetch `/api/student-list`

--- public/src/ts/admin/script.ts
+++ public/src/ts/admin/script.ts
@@ -24,3 +24,36 @@ for (const button of Array.from(sidebarButtons)) {
     }
   })
 }
+
+async function fetchStudents(skip = 0, take = 10) {
+  const table =
+    document.querySelector<HTMLDivElement>('#student-list')?.parentElement
+  try {
+    table?.classList.add('hidden')
+    const response = await fetch(`/api/student-list?skip=${skip}&take=${take}`)
+    const collection = await response.json()
+    table?.classList.remove('hidden')
+    return collection
+  } catch (error) {
+    if (error instanceof Error) alert(error.message)
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function printTable(data: any[]) {
+  const container = document.querySelector<HTMLDivElement>('#student-list')
+  const output = data.map((item) => {
+    const row = document.createElement('tr')
+    const email = document.createElement('td')
+    const name = document.createElement('td')
+    email.className = 'px-4 py-2 border border-gray-100'
+    email.append(document.createTextNode(item.email))
+    name.className = 'px-4 py-2 border border-gray-100'
+    name.append(document.createTextNode(item.displayName))
+    row.append(email, name)
+    return row
+  })
+  container?.append(...output)
+}
+
+fetchStudents().then(printTable)
